492729e3f86a55e63b14c8dbb9505640d831eb91
Alejandro Reyes <reyesbalejandroe@gmail.com>
2025-09-29 13:37:29 +0200

    add/ej3

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ let booleano1 = false;
 let booleano2 = true;
 
 //  3.- Crear variable tipo const de nombre PI declarada con valor 3.14
+const PI = 3.14;
 
 //  4.- Crear variable tipo const de nombre TAU declarada con valor 2 veces PI
