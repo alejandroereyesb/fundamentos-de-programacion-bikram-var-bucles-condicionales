155fc746aa861b20a4e8271255095f44f0edbb1f
Alejandro Reyes <reyesbalejandroe@gmail.com>
2025-09-29 13:29:36 +0200

    add/ej1,2

---
+++ index.js
@@ -0,0 +1,11 @@
+// Variables
+//  1.- Crear variable tipo let de nombre variableSinValor declarada sin valor
+let variableSinValor;
+
+//  2.- Crear 2 variables tipo let de nombres booleano1 y booleano2 con valores booleanos
+let booleano1 = false;
+let booleano2 = true;
+
+//  3.- Crear variable tipo const de nombre PI declarada con valor 3.14
+
+//  4.- Crear variable tipo const de nombre TAU declarada con valor 2 veces PI
